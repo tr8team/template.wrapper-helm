6e062deb41cfc7f71c4029e03a1bff2b6f104c7f
kirinnee <kirinnee97@gmail.com>
2023-01-29 20:39:43 +0800

    config: k3d cluster to create a kubeconfig that is compatible with goku

--- scripts/local/create-k3d-cluster.sh
+++ scripts/local/create-k3d-cluster.sh
@@ -6,13 +6,23 @@ set -eou pipefail
 clusterName="playground"
 [ "$input" = '' ] && input="$clusterName"
 
-echo "Attempting to start cluster '$input'..."
+echo "🧬 Attempting to start cluster '$input'..."
 
 # obtain existing cluster
 current="$(k3d cluster ls -o json | jq -r --arg input "${input}" '.[] | select(.name == $input) | .name')"
 if [ "$current" = "$input" ]; then
-	echo "Cluster already exist!"
-	exit
+	echo "⚠️ Cluster already exist!"
+else
+	echo "✅ Cluster does not exist, creating..."
+	k3d cluster create "$input" --config ./config/k3d.yaml
+	echo "🚀 Cluster created!"
 fi
-echo "Cluster does not exist, creating..."
-k3d cluster create "$input" --config ./config/k3d.yaml
+
+echo "🛠 Generating kubeconfig"
+
+mkdir -p "$HOME/.kube/configs"
+mkdir -p "$HOME/.kube/k3dconfigs"
+k3d kubeconfig get "$clusterName" >"$HOME/.kube/k3dconfigs/k3d-$clusterName"
+KUBECONFIG=$(cd ~/.kube/configs && find "$(pwd)"/* | awk 'ORS=":"')$(cd ~/.kube/k3dconfigs && find "$(pwd)"/* | awk 'ORS=":"') kubectl config view --flatten >~/.kube/config
+chmod 644 ~/.kube/config
+echo "✅ Generated kube config file"
