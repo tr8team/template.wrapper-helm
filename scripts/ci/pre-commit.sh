2972eb5402fb5896f33a4f7272f2c23d6fb9404e
ocind <nicolasdarmadja@gmail.com>
2023-09-29 10:42:55 +0800

    fix: incorrect shebang

--- scripts/ci/pre-commit.sh
+++ scripts/ci/pre-commit.sh
@@ -1,6 +1,2 @@
-#!/nix/var/nix/profiles/default/bin/nix-shell
-#!nix-shell ../../nix/shells.nix -A ci -i bash
-# shellcheck shell=bash
-set -euo pipefail
-pls setup
+#!/bin/sh
 time pre-commit run --all-files -v
